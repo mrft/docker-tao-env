4d12f538a01c98312fe4956c011974ba425f4a2e
Frederik Tilkin <frederik.tilkin@vsko.be>
2018-08-13 15:32:17 +0200

    The init.sh script will now create a sub folder called ./docker_volumes where the volumes will be mapped (using the current folder made things quite hard to understand and messy, even overwriting the README). The .gitignore file will ignore this ..docker_volumes subfolder entirely. Also added a info.php that could be copied to ./docker_volumes/tao/var_www_html to make it easy to check the actual php settings. The files generated based on the .template files will be put at the right place under ./docker_volumes. Oh, and in docker-compose.yml, I used my own fork of the tao repo, and I used postgres as the DB, because I couldn't get mysql to work, and I hate mysql anyway. postgres username = postgres, password = PROJECT_NAME (= name of the folder you cloned this repo to). I guess that's all, oh yes, and I still have to update the readme.md to reflect all these changes.

--- init.sh
+++ init.sh
@@ -8,9 +8,17 @@ fi
 
 PROJECT_NAME=`basename "$PWD"`
 
+TAO_VOLUME_PATH='./docker_volumes/tao'
+NGINX_VOLUME_PATH='./docker_volumes/web'
+
+mkdir -m 0755 -p "${NGINX_VOLUME_PATH}"
+mkdir -m 0755 -p "${TAO_VOLUME_PATH}/var_www_html"
+cp info.php "${TAO_VOLUME_PATH}/var_www_html"
+
+
 # Using | instead of / because $PWD contains /'s
 sed "	s|PROJECT_ROOT_VALUE|$PWD|g 
-	s|PROJECT_NAME_VALUE|$PROJECT_NAME|g" nginx-proxy.conf.template > nginx-proxy.conf
-sed "s|PROJECT_NAME_VALUE|$PROJECT_NAME|g" nginx.conf.template > nginx.conf
+	s|PROJECT_NAME_VALUE|$PROJECT_NAME|g" nginx-proxy.conf.template > ${TAO_VOLUME_PATH}/nginx-proxy.conf
+sed "s|PROJECT_NAME_VALUE|$PROJECT_NAME|g" nginx.conf.template > ${NGINX_VOLUME_PATH}/nginx.conf
 sed "	s|NGINX_PORT=80|NGINX_PORT=$NGINX_PORT|g
-	s|PROJECT_NAME_VALUE|$PROJECT_NAME|g" .env.template > .env
+	s|PROJECT_NAME_VALUE|$PROJECT_NAME|g" .env.template > ${TAO_VOLUME_PATH}/.env
